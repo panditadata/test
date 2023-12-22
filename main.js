cef4cf3859e7f96c3548bd470128c6f781211224
Pandita Data <panditadata@gmail.com>
2023-12-22 13:13:13 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function fetchEarthquakeData() {
+    const apiUrl = 'https://example.com/api/earthquakes'; // Replace with the actual API endpoint or website URL
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => displayEarthquakeData(data))
+        .catch(error => console.error('Error fetching earthquake data:', error));
+}
+
+function displayEarthquakeData(data) {
+    const earthquakeDataContainer = document.getElementById('earthquakeData');
+    earthquakeDataContainer.innerHTML = '';
+
+    if (data && data.features) {
+        data.features.forEach(feature => {
+            const earthquakeInfo = `
+                <p>Magnitude: ${feature.properties.mag}</p>
+                <p>Location: ${feature.properties.place}</p>
+                <p>Date and Time: ${new Date(feature.properties.time).toLocaleString()}</p>
+                <hr>
+            `;
+            earthquakeDataContainer.innerHTML += earthquakeInfo;
+        });
+    } else {
+        earthquakeDataContainer.innerHTML = '<p>No earthquake data available.</p>';
+    }
+}
