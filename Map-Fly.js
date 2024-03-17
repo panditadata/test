82022f9efe500b8b8cce44afb7f7b05cd7a147e4
Pandita Data <panditadata@gmail.com>
2024-03-17 10:48:51 +0400

    Add files via upload

--- Map-Fly.js
+++ Map-Fly.js
@@ -9,20 +9,20 @@ var mapOptions = {
 var map = new L.Map('map', mapOptions);
 var fullScreen = new L.Control.FullScreen(); 
 map.addControl(fullScreen);
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(map);
-var helicopterIcon = L.Icon.extend({
+var flyIcon = L.Icon.extend({
 options: {
-    iconUrl: 'pilot-helicopter.gif',
+   
 
-    iconSize:     [50, 49], // size of the icon
-
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-
-    popupAnchor:  [-3, -76]
+    iconSize:     [50, 49] // size of the icon]
 }
 });
+
+var helicopterIcon = new flyIconIcon({iconUrl: 'pilot-helicopter.gif'}),
+    redIcon = new LeafIcon({iconUrl: 'leaf-red.png'}),
+    orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
 
 L.marker([51.5, -0.09], {iconUrl: helicopterIcon}).addTo(map);
 
