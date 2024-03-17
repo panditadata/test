f44504c235a48ac204ba7931508ff6bda2b08aec
Pandita Data <panditadata@gmail.com>
2024-03-17 21:36:49 +0400

    Add files via upload

--- Map-Fly.js
+++ Map-Fly.js
@@ -1,6 +1,6 @@
 var mapOptions = {
-    center: [20,0],
-    zoom: 3,
+    center: [20,-91.83],
+    zoom: 4,
     dragging: true,
     doubleClickZoom:true,
     
@@ -23,7 +23,7 @@ var large_airportIcon = new flyIcon({iconUrl: 'airplane-joypixels-flying.gif'});
 var medium_airportIcon = new flyIcon({iconUrl: 'landing-touch-down-landing.gif'});
 var small_airportIcon = new flyIcon({iconUrl: 'takeoff-plane with shadow.gif'});
 var seaplane_baseIcon = new flyIcon({iconUrl: 'giphy.gif'});
-var closedIcon = new flyIcon({iconUrl: 'closed airport.jpeg'});
+var closedIcon = new flyIcon({iconUrl: 'closed airport.JPEG'});
 
 var helicopterMarker = L.marker([40.07080078125, -74.9336013793945], {icon: helicopterIcon}).addTo(map);
 helicopterMarker.bindPopup('Total Rf Heliport');
@@ -47,10 +47,10 @@ var large_airportIcon = L.marker([-9.44338035583496, 147.220001220703], {icon: l
 large_airportIcon.bindPopup('Halifax / Stanfield International Airport');
 
 var another3_large_airportIcon = L.marker([44.88079834	-63.50859833], {icon: large_airportIcon}).addTo(map);
-another3_large_airportIcon_airportIcon.bindPopup('Port Moresby Jacksons International Airport');
+another3_large_airportIcon.bindPopup('Port Moresby Jacksons International Airport');
 
-var another1_airportIcon = L.marker([45.32249832	-75.66919708], {icon: large_airportIcon}).addTo(map);
-another1_airportIcon.bindPopup('Ottawa Macdonald-Cartier International Airport');
+var another1_large_airportIcon = L.marker([45.32249832	-75.66919708], {icon: large_airportIcon}).addTo(map);
+another1_large_airportIcon.bindPopup('Ottawa Macdonald-Cartier International Airport');
 
 
 var another_helicopterIcon = L.marker([32.7273736, -116.4597417], {icon: helicopterIcon}).addTo(map);
