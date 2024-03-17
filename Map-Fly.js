082d3a50a9962239cd561d180e7e0d09e0c56129
Pandita Data <panditadata@gmail.com>
2024-03-17 11:26:03 +0400

    Add files via upload

--- Map-Fly.js
+++ Map-Fly.js
@@ -11,17 +11,7 @@ var map = new L.Map('map', mapOptions);
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(map);
-var flyIcon = L.Icon.extend({
-options: {
-   
 
-    iconSize:     [50, 49] // size of the icon]
-}
-});
-
-var helicopterIcon = new flyIcon({iconUrl: 'pilot-helicopter.gif'});
-
-L.marker([51.5, -0.09], {icon: helicopterIcon}).addTo(map);
 
 // Fullscreen control
 var fullScreen = new L.Control.FullScreen(); 
